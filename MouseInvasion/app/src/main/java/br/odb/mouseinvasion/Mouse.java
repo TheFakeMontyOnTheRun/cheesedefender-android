629e37213384960da22705b91c882f3cc7d15ec3
Daniel Monteiro <danielmonteiro@id.uff.br>
2020-01-19 14:43:21 +0000

    Make mouse ship explosions contract just as fast as the missile ones
    Maybe it should contract twice as fast?

--- MouseInvasion/app/src/main/java/br/odb/mouseinvasion/Mouse.java
+++ MouseInvasion/app/src/main/java/br/odb/mouseinvasion/Mouse.java
@@ -66,7 +66,7 @@ public class Mouse extends GameObject implements Explosive {
 		}
 
 		if (isExploding()) {
-			explosionTime -= 3 * delta;
+			explosionTime -= delta;
 
 			if (!isExploding()) {
 				active = false;
